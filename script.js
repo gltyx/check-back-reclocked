f90758c06305a70bfe4d8e8ea6d6a91708ccab0d
MarcElAcoplao <marcrams2007@gmail.com>
2026-02-23 18:43:54 +0100

    mention singularity^52 esc now closes tabs

--- script.js
+++ script.js
@@ -340,3 +340,25 @@ function gridInitializer() { //Ignore this, it was a test, you might not see thi
     }
 }
 gridInitializer()
+
+// Meta function guaranteed to run after the DOM is ready
+function onDomReady(fn) {
+    // see if DOM is already available
+    if (document.readyState === "complete" || document.readyState === "interactive") {
+        // call on next available tick
+        setTimeout(fn, 1);
+    } else {
+        document.addEventListener("DOMContentLoaded", fn);
+    }
+}
+
+onDomReady(function () {
+    document.addEventListener("keydown", function(event) {
+        const key = event.key; // Or const {key} = event; in ES6+
+        if (key === "Escape") {
+            if (document.getElementById("petsDiv").style.display == "block") {openClosePetsTab()}
+            if (document.getElementById("unboxDiv").style.display == "block") {openCloseUnboxTab()}
+            if (document.getElementById("statsDiv").style.display == "block") {openCloseStatsTab()}
+        }
+    });
+});
